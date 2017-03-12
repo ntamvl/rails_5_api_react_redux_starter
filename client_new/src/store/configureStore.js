038288e52e2a59faf1e12c4befbaf487c2f9f7b3
Tam Nguyen <ntamvl@gmail.com>
2017-03-12 13:52:43 +0700

    clean code

--- client_new/src/store/configureStore.js
+++ client_new/src/store/configureStore.js
@@ -2,15 +2,12 @@ import { createStore, applyMiddleware, combineReducers, compose } from "redux";
 import thunkMiddleware from "redux-thunk";
 import createLogger from "redux-logger";
 import auth from "../reducers/auth";
-import { selectedUsersPage, usersByPage } from "../reducers/users";
 import { selectedReposPage, reposByPage } from "../reducers/repos";
 import { usersSelectedPage, usersPage, newUser } from '../reducers/user';
 
 const logger = createLogger();
 const rootReducer = combineReducers({
   auth,
-  selectedUsersPage,
-  usersByPage,
   selectedReposPage,
   reposByPage,
   usersSelectedPage,
